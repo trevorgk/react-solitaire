f6a5851053ca92d346282587b16bd44fa1b671af
Trevor K <trevorgk@gmail.com>
2015-08-16 17:35:54 +1000

    overlay on click

--- src/components/PlayingCard.js
+++ src/components/PlayingCard.js
@@ -1,11 +1,39 @@
 import React, { PropTypes } from 'react';
 
-export default class PlayingCard extends React.Component{
-    render() {
-        return (
-            <div className="PlayingCard" style={this.props.style}>
-                <img style={{width:"80px"}} src={this.props.card.display()} />
-            </div>
-        );
-    }
+export default class PlayingCard extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {isSelected: false};
+  }
+
+  renderOverlay(color) {
+    return (
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: '100%',
+        zIndex: 1,
+        opacity: 0.5,
+        backgroundColor: color,
+      }} />
+    );
+  }
+
+  handleClick(event) {
+      this.setState({isSelected:!this.state.isSelected});
+      console.log('isSelected',this.state.isSelected);
+  };
+
+  render() {
+      let style = React.addons.update({position: "relative", width:"80px",height:"112px"}, {$merge: this.props.style});
+      return (
+          <div className="PlayingCard" onClick={this.handleClick.bind(this)} style={style}>
+              <img style={{width:"100%"}} src={this.props.card.display()} />
+              {this.state.isSelected && this.renderOverlay('aquamarine')}
+          </div>
+      );
+  }
 }
