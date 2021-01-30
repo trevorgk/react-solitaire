8fa922bfd0e5b823c0431a5b8e57efa01e0c9349
Trevor Kilvington <trevorgk@gmail.com>
2021-01-30 21:43:54 +1100

    Add basic components

---
+++ src/components/GameBoard/GameBoard.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const GameBoard = () => <div>foo</div>;
+
+export default GameBoard;
---
+++ src/components/PlayingCard/PlayingCard.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const PlayingCard = () => <div>foo</div>;
+
+export default PlayingCard;
