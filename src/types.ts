7876fcea1f9787d07d38e3071fa6afad65de42e2
Trevor Kilvington <trevorgk@gmail.com>
2021-01-30 21:43:39 +1100

    Add playing card model

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export const suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts'] as const;
+export const pips = [
+  'Ace',
+  'Two',
+  'Three',
+  'Four',
+  'Five',
+  'Six',
+  'Seven',
+  'Eight',
+  'Nine',
+  'Ten',
+  'Jack',
+  'Queen',
+  'King',
+] as const;
+export const colours = ['Red', 'Black'];
+
+export type Pip = typeof pips[number];
+export type Suit = typeof suits[number];
+export type Colour = typeof colours[number];
