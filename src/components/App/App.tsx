4f6e462847f9e3f313da39faee7a4101f938d7e6
Trevor Kilvington <trevorgk@gmail.com>
2021-01-31 19:11:19 +1100

    Make cards a wee bit smaller

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles.css';
 import GameBoard from '../GameBoard/GameBoard';
 
 const App = () => (
--- src/components/CardDisplay/CardDisplay.tsx
+++ src/components/CardDisplay/CardDisplay.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles.css';
 import { PlayingCard } from '../../types';
 import { getSrc, getAltText } from './utils';
 
